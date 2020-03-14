fe7542ea972881d7cfa2ec44f28cc6f027f44db1
QuentinSauvage <sauvage.quentin62@gmail.com>
2020-03-14 17:31:15 +0100

    Added targets in order to finish level

--- Source/FiftyMinInside/FiftyMinInsideGameMode.cpp
+++ Source/FiftyMinInside/FiftyMinInsideGameMode.cpp
@@ -7,4 +7,30 @@ AFiftyMinInsideGameMode::AFiftyMinInsideGameMode()
 {
 	// set default pawn class to our flying pawn
 	DefaultPawnClass = AFiftyMinInsidePawn::StaticClass();
+
+}
+
+int AFiftyMinInsideGameMode::GetTargetCounter()
+{
+	return TargetCounter;
+}
+
+void AFiftyMinInsideGameMode::SetTargetCounter(int NewTargetCounter)
+{
+	TargetCounter = NewTargetCounter;
+
+	if (!TargetCounter && !PickUpsCounter)
+	{
+		this->OnLevelCleared();
+	}
+}
+
+int AFiftyMinInsideGameMode::GetPickUpsCounter()
+{
+	return PickUpsCounter;
+}
+
+void AFiftyMinInsideGameMode::SetPickUpsCounter(int NewPickUpsCounter)
+{
+	PickUpsCounter = NewPickUpsCounter;
 }
--- Source/FiftyMinInside/FiftyMinInsideGameMode.h
+++ Source/FiftyMinInside/FiftyMinInsideGameMode.h
@@ -11,8 +11,27 @@ class AFiftyMinInsideGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
+	int TargetCounter;
+	int PickUpsCounter;
+
 public:
 	AFiftyMinInsideGameMode();
+
+	UFUNCTION(BlueprintCallable, Category = Health)
+	int GetTargetCounter();
+
+	UFUNCTION(BlueprintCallable, Category = Health)
+	void SetTargetCounter(int NewTargetCounter);
+	
+	UFUNCTION(BlueprintCallable, Category = Health)
+	int GetPickUpsCounter();
+	
+	UFUNCTION(BlueprintCallable, Category = Health)
+	void SetPickUpsCounter(int NewPickUpsCounter);
+
+protected:
+	UFUNCTION(BlueprintImplementableEvent, Category = EndGame)
+	void OnLevelCleared();
 };
 
 
